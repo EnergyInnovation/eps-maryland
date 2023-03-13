d12d358c33faa539ae2b5ecdd19459f12dcd348f
Daniel O'Brien <dobrien@energyinnovation.org>
2023-03-13 15:09:37 -0400

    Updates to ICCT input data to account for entities of concern requirement

--- GeneratedDataLoggingScript.cmd
+++ GeneratedDataLoggingScript.cmd
@@ -1,10 +1,22 @@
 SPECIAL>LOADMODEL|"EPS.mdl"
 
-SIMULATE>SAVELIST|OutputVarsToExport-Calibration2.lst
+SIMULATE>SAVELIST|OutputVarsToExport.lst
 SIMULATE>RUNNAME|NoSettings
 SIMULATE>READCIN|
 MENU>RUN|O
-MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport-Calibration2.lst|+!||2020|2050|:NoSettings
+MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport.lst|+!||2020|2050|:NoSettings
 FILE>DELETE|NoSettings.vdfx
+
+SIMULATE>RUNNAME|IRA_Moderate_Incent
+SIMULATE>READCIN|IRA_Moderate_Incentives
+MENU>RUN|O
+MENU>VDF2TAB|IRA_Moderate_Incent.vdfx|IRA_Moderate_Incent.tsv|OutputVarsToExport.lst|+!||2020|2050|:IRA_Moderate_Incent
+FILE>DELETE|IRA_Moderate_Incent.vdfx
+
+SIMULATE>RUNNAME|IRA_A
+SIMULATE>READCIN|IRA_ACCII
+MENU>RUN|O
+MENU>VDF2TAB|IRA_A.vdfx|IRA_A.tsv|OutputVarsToExport.lst|+!||2020|2050|:IRA_A
+FILE>DELETE|IRA_A.vdfx
 
 SIMULATE>SAVELIST|
